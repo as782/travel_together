32eaa523087b83e0d5abc1dc575dc00d095cd76a
wengkaibin <617367465@qq.com>
2024-03-07 12:56:40 +0800

    Init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# travel_together
+ 一个基于Vue.js和Node.js的旅游社交网站
---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template>
+  <div id="app">
+    <van-button plain type="primary">朴素按钮</van-button>
+    <van-button plain type="success">朴素按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="success">成功按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+
+    <div class="text-3xl font-bold underline bg-red-500">
+      Hello world!
+    </div>
+    <div>
+      Hello world!
+    </div>
+  </div>
+</template>
+
+<style scoped></style>

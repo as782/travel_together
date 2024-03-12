35976a708ba52d5062e8618c01bc8bf72def3774
wengkaibin <617367465@qq.com>
2024-03-12 16:42:40 +0800

    chore:add area-list package

---
+++ src/views/error/404Page.vue
@@ -0,0 +1,15 @@
+<template>
+    <main class="flex flex-col justify-center items-center">
+        <h1>404</h1>
+        <p>Page not found</p>
+        <van-button @click="router.back()" type="warning">go back</van-button>
+    </main>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+</script>
+
+<style scoped></style>
